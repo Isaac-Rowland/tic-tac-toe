09dab9b0d8151795f2b30dbab87151e99cf40b87
Isaac <ibirowland@gmail.com>
2022-02-03 09:24:47 +1100

    everythings still working

--- tictactoe.js
+++ tictactoe.js
@@ -5,14 +5,13 @@
 // [topl, midm, botr]
 // [botl, midm, topr]
 // vars
-var input = document.querySelector("p")
 var inputresponse = document.querySelector(".inputMessage")
 var allPlayers = ["X","O"]
 var currentPlayer = allPlayers[0]
 
 
 
-//Whose turn is it
+// Whose turn is it
 function togglePlayer(){
     if (currentPlayer === allPlayers[0]){
         currentPlayer = allPlayers[1]
@@ -24,41 +23,48 @@ function togglePlayer(){
 
 // Is there a winner? 
 function isThereAWinner(){
-    if (topL.textContent === "X" && topM.textContent === "X" && topR.textContent === "X" ||midL.textContent === "X" && midM.textContent === "X" && midR.textContent === "X" || botL.textContent === "X" && botM.textContent === "X" && botR.textContent === "X" || topL.textContent === "X" && midM.textContent === "X" && botR.textContent === "X" ||botL.textContent === "X" && midM.textContent === "X" && topR.textContent === "X"){
-        inputresponse.textContent = "Player 1 wins"
-    }
-    else if (topL.textContent === "O" && topM.textContent === "O" && topR.textContent === "O" ||midL.textContent === "O" && midM.textContent === "O" && midR.textContent === "O" || botL.textContent === "O" && botM.textContent === "O" && botR.textContent === "O" || topL.textContent === "O" && midM.textContent === "O" && botR.textContent === "O" ||botL.textContent === "O" && midM.textContent === "O" && topR.textContent === "O"){
-        inputresponse.textContent = "Player 2 wins"
+    if (topL.textContent === currentPlayer && topM.textContent === currentPlayer && topR.textContent === currentPlayer ||midL.textContent === currentPlayer && midM.textContent === currentPlayer && midR.textContent === currentPlayer || botL.textContent === currentPlayer && botM.textContent === currentPlayer && botR.textContent === currentPlayer || topL.textContent === currentPlayer && midM.textContent === currentPlayer && botR.textContent === currentPlayer ||botL.textContent === currentPlayer && midM.textContent === currentPlayer && topR.textContent === currentPlayer){
+        inputresponse.textContent = `Player ${allPlayers.indexOf(currentPlayer) + 1} wins`
     }
 }
-//which box has been clicked
+// which box has been clicked
 function boxClicked (event) {
     var thisBox = event.target
-    if(thisBox.textContent === "X" || thisBox.textContent === "O"){
+    // TODO should I use includes or indexOf
+    if(allPlayers.includes(thisBox.textContent)){
         inputresponse.textContent = "Pick another box pls"
     }
     else {
         inputresponse.textContent = "Nice spot"
         thisBox.textContent = currentPlayer
-        isThereAWinner()
+        isThereAWinner(currentPlayer)
         togglePlayer()
     }
 }
-var topL = document.querySelector(".topL")
+var topL = document.querySelector("#topL")
 topL.addEventListener("click", boxClicked)
-var topM = document.querySelector(".topM")
+var topM = document.querySelector("#topM")
 topM.addEventListener("click", boxClicked)
-var topR = document.querySelector(".topR")
+var topR = document.querySelector("#topR")
 topR.addEventListener("click", boxClicked)
-var midL = document.querySelector(".midL")
+var midL = document.querySelector("#midL")
 midL.addEventListener("click", boxClicked)
-var midM = document.querySelector(".midM")
+var midM = document.querySelector("#midM")
 midM.addEventListener("click", boxClicked)
-var midR = document.querySelector(".midR")
+var midR = document.querySelector("#midR")
 midR.addEventListener("click", boxClicked)
-var botL = document.querySelector(".botL")
+var botL = document.querySelector("#botL")
 botL.addEventListener("click", boxClicked)
-var botM = document.querySelector(".botM")
+var botM = document.querySelector("#botM")
 botM.addEventListener("click", boxClicked)
-var botR = document.querySelector(".botR")
+var botR = document.querySelector("#botR")
 botR.addEventListener("click", boxClicked)
+
+var restartButton = document.querySelector(".resetbutton")
+// Restart game button 
+// function RestartGame(){
+    restartButton.addEventListener("click", function (){
+        // reset all divs to have nothing in them. 
+    var allBoxes = document.querySelectorAll(".boxes").textContent = ""
+    })
+// }
